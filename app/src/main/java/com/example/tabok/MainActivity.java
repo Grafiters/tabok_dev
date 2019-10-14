a5234824441199555c3788309a34cf3bf70feb7f
Grafiters <bayugrafit@gmail.com>
2019-10-14 20:44:12 +0700

    try camera scan

--- app/src/main/java/com/example/tabok/MainActivity.java
+++ app/src/main/java/com/example/tabok/MainActivity.java
@@ -2,13 +2,31 @@ package com.example.tabok;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static TextView resultView;
+    Button scanBut;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        resultView = (TextView)findViewById(R.id.result);
+        scanBut = (Button)findViewById(R.id.scan);
+
+        scanBut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), scanView.class));
+            }
+        });
+
     }
 }
