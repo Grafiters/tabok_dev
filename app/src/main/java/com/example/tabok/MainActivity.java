268c5bdd339470a5ebd045b6ee61f5f122d459fb
novan <novan.adhagara@gmail.com>
2019-10-20 15:11:16 +0700

    fix scanner resources to scan QR-code

--- app/src/main/java/com/example/tabok/MainActivity.java
+++ app/src/main/java/com/example/tabok/MainActivity.java
@@ -2,44 +2,60 @@ package com.example.tabok;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
 import android.os.Bundle;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.zxing.Result;
-
-import org.w3c.dom.Text;
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionDeniedResponse;
+import com.karumi.dexter.listener.PermissionGrantedResponse;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.single.PermissionListener;
 
 import me.dm7.barcodescanner.zxing.ZXingScannerView;
 
+public class MainActivity extends AppCompatActivity implements ZXingScannerView.ResultHandler {
 
-public class MainActivity extends AppCompatActivity implements ZXingScannerView.ResultHandler{
-
-    private ZXingScannerView mScannerView;
+    private ZXingScannerView scannerView;
+    private TextView txtResult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        generate();
-    }
 
-    private void generate(){
-        mScannerView = new ZXingScannerView(this);
-        setContentView(mScannerView);
-        mScannerView.setResultHandler(this);
-        mScannerView.startCamera();
-    }
+        scannerView = (ZXingScannerView)findViewById(R.id.scannerView);
+        txtResult = (TextView)findViewById(R.id.text);
 
-    protected void onPause(){
-        super.onPause();
-        mScannerView.stopCamera();
+        Dexter.withActivity(this).withPermission(Manifest.permission.CAMERA).withListener(new PermissionListener() {
+            @Override
+            public void onPermissionGranted(PermissionGrantedResponse response) {
+                scannerView.setResultHandler(MainActivity.this);
+                scannerView.startCamera();
+            }
+
+            @Override
+            public void onPermissionDenied(PermissionDeniedResponse response) {
+                Toast.makeText(MainActivity.this,"You Must to allow this permission", Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onPermissionRationaleShouldBeShown(PermissionRequest permission, PermissionToken token) {
+
+            }
+        }).check();
+    }
+    protected void onDestroy(){
+        scannerView.stopCamera();
+        super.onDestroy();
     }
 
     @Override
     public void handleResult(Result result) {
-        String text, format;
-        text = result.getText().toString();
-        format = result.getBarcodeFormat().toString();
-        Toast.makeText(getApplicationContext(), "Text : " + text + "\n" + "Format : "+format, Toast.LENGTH_LONG).show();
+
+        txtResult.setText(result.getText());
     }
 }
